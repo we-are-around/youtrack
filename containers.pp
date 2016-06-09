67d51dfb6481bf09824a641c8351dbb17e46c5e4
we-are-around <info@we-are-around.com>
2016-06-09 14:48:52 -0300

    from cmd #GIT-1 test

--- containers.pp
+++ containers.pp
@@ -4,14 +4,14 @@
   env	       => ['APACHE_LOG_DI=R"/var/log/httpd"', 'APACHE_PID_FILE="/var/run/httpd/httpd.pid"'],
   volumes      => ['/docker-data/www/:/usr/local/apache2/htdocs'],
   hostname     => 'webserver',
-}\
+}
 
 docker::run { 'dbserver':\
   image        => 'mysql:latest',
   env	       => ['MYSQL_ROOT_PASSWORD=crossover', 'MYSQL_USER=crossover', 'MYSQL_PASSWORD=crossover', 'MYSQL_DATABASE=crossover'],
   ports        => ['3006:3006'],
   hostname     => 'dbserver',
-}\
+}
 
 docker::run { 'monitoring':\
   image        => 'icinga/icinga2',
