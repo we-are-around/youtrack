f11626c32c168a99c5bb2f8d3f57745be927e7d6
Around Admin <info@we-are-around>
2016-06-13 09:46:51 -0300

    backup commit added

--- backup_containers.sh
+++ backup_containers.sh
@@ -16,13 +16,19 @@ hub_usr="wearearound"
 # Log on to Docker Hub
 docker login --username $hub_usr --password $hub_pwd
 
-# Archive RUNNING Container on DockerHUB
+# prepare dinamic image and container variables
 image=$(docker images|grep $repo|grep latest|awk '{print $3}')	
 container_id=$(docker ps |grep $container|awk '{print $1}')
+
+# Archive week day RUNNING Container on DockerHUB
 docker rmi $repo:backup-$(date +%u)
 docker commit -p -a "Francisco Palacios" -m "Dayly Backup" $container $repo:backup-$(date +%u)
+docker push wearearound/youtrack:backup-$(date +%u)
+
+# Archive latest RUNNING Container on DockerHUB
 docker rmi $repo:latest
 docker commit -p -a "Francisco Palacios" -m "Dayly Backup" $container $repo:latest
+docker push wearearound/youtrack:latest
 
 # Local Archive 
 #docker save -o $container_store_path/$container.$(date	+%F).tar $image
--- youtrack.pp
+++ youtrack.pp
@@ -1,5 +1,5 @@
 docker::run { 'around-youtrack':
-  image        => 'zoer/youtrack',
+  image        => 'wearearound/youtrack',
   ports        => ['80:80'],
   hostname     => 'around-youtrack',
   name	       => 'around-youtrack',
