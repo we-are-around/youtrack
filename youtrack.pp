97bfeefeaabd6d97d3b97c34f63d581bc49a4d6c
Around Admin <info@we-are-around>
2016-06-13 10:51:36 -0300

    Add Jira scripts

--- youtrack.pp
+++ youtrack.pp
@@ -1,5 +1,5 @@
 docker::run { 'around-youtrack':
-  image        => 'wearearound/youtrack',
+  image        => 'wearearound/youtrack:backup-1',
   ports        => ['80:80'],
   hostname     => 'around-youtrack',
   name	       => 'around-youtrack',
