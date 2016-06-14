ca24f75101c9fcd54bfe649877d54d4f3598d369
Around Admin <info@we-are-around>
2016-06-14 09:23:47 -0300

    working project add script

--- aroundCreateProject.py
+++ aroundCreateProject.py
@@ -11,6 +11,8 @@ def main(argv):
    project_usr = 'root'
    project_pwd = 'Sarasa12'
    svn_cli = pysvn.Client()
+   svn_working_path = '/tmp'
+   youtrack_url = 'http://localhost:80'
    try:
       opts, args = getopt.getopt(argv,"hp:i:d:",["project_name=","project_id=", "project_desc="])
    except getopt.GetoptError:
@@ -27,13 +29,17 @@ def main(argv):
       elif opt in ("-d", "--project_desc"):
          project_desc = arg
 
-   #yt = Connection('http://v-rot-mne-nogi.myjetbrains.com/youtrack', 'root', 'root') #, proxy_info = httplib2.ProxyInfo(socks.PROXY_TYPE_HTTP, 'localhost', 8888))
-   yt = Connection('http://localhost:80', project_usr, project_pwd)
+   #yt = Connection('http://v-rot-mne-nogi.myjetbrains.com/youtrack', 'root', 'root') 
+   #, proxy_info = httplib2.ProxyInfo(socks.PROXY_TYPE_HTTP, 'localhost', 8888))
+   yt = Connection(youtrack_url, project_usr, project_pwd)
 
+   #def createProjectDetailed(self, projectId, name, description, projectLeadLogin, startingNumber=1)
+   print yt.createProjectDetailed(project_id, project_name, project_desc, project_usr, 1)
 
-   #print yt.createProjectDetailed('youtrack', 'resttest', 'test', 'test', '1', 'Bug', 'Unknown', 'Open', '', '', '')
-   #yt.createProjectDetailed(yt, project_name, project_id, project_desc, project_usr, 1)
-   yt.createProjectDetailed(project_name, project_id, project_desc, project_usr, 1)
+   #Create SVN Project with pysvn   
+   #svn_cli.add(svn_working_path/project_name)
+   #committing the change actually adds the file to the repository
+   #svn_cli.checkin(svn_working_path, 'Adding a Directory')
 
 if __name__ == "__main__":
    main(sys.argv[1:])
